60c77418491360de110f2af7681f68ef0a35f09a
muhammadashariabdillah24 <tutordunet09@gmail.com>
2021-08-16 09:06:25 +0700

    Aplikasi Jadwal Sholat

---
+++ app.js
@@ -0,0 +1,54 @@
+function prayerTimes(latitude, longitude){
+    fetch('http://api.aladhan.com/v1/calendar?latitude='+latitude+'&longitude='+longitude+'&method=4')
+    .then(response => response.json())
+    .then(function(response) {
+        let date        = new Date()
+        let today       = date.getDate() - 1
+        let data        = response.data[0].timings
+
+        let app         = document.getElementById('app')
+        let table       = document.createElement('table')
+        let tableTbody  = document.createElement('tbody')
+        
+        for(i in data){
+            let row         = tableTbody.insertRow()
+            let name        = row.insertCell(0)
+            let time        = row.insertCell(1)
+            name.innerHTML  = i
+            time.innerHTML  = data[i]
+            tableTbody.appendChild(row)
+        }
+        table.appendChild(tableTbody)
+        app.appendChild(table)
+    })
+}
+
+function success(position) {
+    prayerTimes(position.coords.latitude, position.coords.longitude)
+}
+
+function error() {
+    // Default latitude dan longtitude
+    prayerTimes('-7.797068','110.370529')
+}
+
+function userLocation() {
+    if (!navigator.geolocation) {
+        alert('Geolocation Tidak Didukung Oleh Browser Anda! Silahkan Gunakkan Browser lain')
+    } else {
+        navigator.geolocation.getCurrentPosition(success, error)
+    }
+}
+
+function index(){
+
+    let app         = document.getElementById('app')
+    let h3          = document.createElement('h3')
+    h3.innerHTML    = 'Jadwal Sholat'
+
+    app.appendChild(h3)
+
+    userLocation();
+}
+
+index()
